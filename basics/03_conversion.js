06f356cf8d33881fd49c86758d5baab38fb00345
harshitwork1321 <harshitwork1321@gmail.com>
2026-01-12 19:00:15 +0000

    data types

--- basics/03_conversion.js
+++ basics/03_conversion.js
@@ -17,3 +17,21 @@ let booleanIsLoggedIn = Boolean(isLoggedIn)
 console.log(booleanIsLoggedIn);
 // 1=> true; 0=> false
 // ""=> false; "harshit"=> true;
+
+
+
+// =======================Operations=======================//
+
+
+// let value="3"
+// let negvalue= -value
+// console.log(negvalue);
+// console.log(typeof negvalue);
+
+
+console.log(+true)
+
+console.log("2"+1);
+console.log("02"+3);
+console.log(1+2+"3");
+console.log("1"+2+3);
